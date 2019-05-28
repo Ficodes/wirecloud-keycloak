35db29071b77b25ac396da0144bee9fb967779d1
Francisco de la Vega <fdelavega@ficodes.com>
2019-05-28 12:37:44 +0200

    Restructure package to enable installation with pip

--- wirecloud/keycloak/proxy.py
+++ wirecloud/keycloak/proxy.py
@@ -17,6 +17,25 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
 
+from io import BytesIO
+import time
+
+from django.conf import settings
+from django.utils.http import urlquote_plus
+from django.utils.translation import ugettext as _
+
+from wirecloud.fiware import FIWARE_LAB_CLOUD_SERVER
+from wirecloud.fiware.openstack_token_manager import OpenstackTokenManager
+from wirecloud.fiware.plugins import IDM_SUPPORT_ENABLED
+from wirecloud.proxy.utils import ValidationError
+
+
+if IDM_SUPPORT_ENABLED:
+    from social_django.utils import load_strategy
+    STRATEGY = load_strategy()
+else:
+    STRATEGY = None
+
 
 class IDMTokenProcessor(object):
     pass
