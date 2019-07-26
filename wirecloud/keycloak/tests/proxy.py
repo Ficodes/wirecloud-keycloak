5d4b8113c3c9fa522be51374fb00764599612234
Francisco de la Vega <fdelavega@ficodes.com>
2019-07-26 14:27:36 +0200

    Add test to validate error in proxy class

--- wirecloud/keycloak/tests/proxy.py
+++ wirecloud/keycloak/tests/proxy.py
@@ -18,10 +18,13 @@
 # along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
 
 from importlib import reload
+from io import BytesIO
 
 import unittest
 from unittest import TestCase
 from unittest.mock import patch, MagicMock, Mock
+
+from wirecloud.proxy.utils import ValidationError
 
 
 def translation_mock(text):
@@ -35,6 +38,7 @@ class IDMTokenProcessorTestCase(TestCase):
         reload(wirecloud.keycloak.proxy)
 
         wirecloud.keycloak.proxy._ = translation_mock
+        wirecloud.keycloak.proxy.IDM_SUPPORT_ENABLED = True
         self._strategy = MagicMock()
         wirecloud.keycloak.proxy.STRATEGY = self._strategy
 
@@ -114,9 +118,11 @@ class IDMTokenProcessorTestCase(TestCase):
                 'fiware-oauth-token': 'token1',
                 'fiware-oauth-header-name': 'Authorization',
                 'fiware-oauth-source': 'workspaceowner',
-                'fiware-oauth-get-parameter': 'x-api-key'
+                'fiware-oauth-get-parameter': 'x-api-key',
+                'fiware-oauth-body-pattern': '{token}'
             },
-            'workspace': workspace
+            'workspace': workspace,
+            'data': BytesIO('The token is {token}'.encode('utf8'))
         }
 
         self._text_proxy_processor(request)
@@ -127,10 +133,93 @@ class IDMTokenProcessorTestCase(TestCase):
         oauth_info.refresh_token.assert_called_once_with(self._strategy)
 
         self.assertEqual({
-            'Authorization': 'Bearer token'
+            'Authorization': 'Bearer token', 'content-length': '18'
         }, request['headers'])
         self.assertEqual('http://proxy.com/url/?id=1&x-api-key=token', request['url'])
+        self.assertEqual(b'The token is token', request['data'].read())
 
+    def _test_validation_error(self, msg, request):
+        error = None
+        try:
+            self._text_proxy_processor(request)
+        except Exception as e:
+            error = e
+
+        self.assertTrue(isinstance(error, ValidationError))
+        self.assertEqual(msg, str(error))
+
+    @patch('django.conf.settings', new=MagicMock(INSTALLED_APPS=()))
+    @patch('wirecloud.keycloak.utils.build_backend', new=MagicMock())
+    @patch('wirecloud.keycloak.utils.build_version_hash', new=MagicMock())
+    @patch('wirecloud.keycloak.utils.load_strategy', new=MagicMock())
+    def test_idm_not_enabled(self):
+        request = {
+            'headers': {
+                'fiware-oauth-token': 'token1',
+                'fiware-oauth-header-name': 'Authorization'
+            },
+            'workspace': {}
+        }
+
+        import wirecloud.keycloak.proxy
+        reload(wirecloud.keycloak.proxy)
+
+        wirecloud.keycloak.proxy._ = translation_mock
+        wirecloud.keycloak.proxy.IDM_SUPPORT_ENABLED = False
+
+        processor = wirecloud.keycloak.proxy.IDMTokenProcessor()
+        error = None
+        try:
+            processor.process_request(request)
+        except Exception as e:
+            error = e
+
+        self.assertTrue(isinstance(error, ValidationError))
+        self.assertEqual('IdM support not enabled', str(error))
+
+    def test_workspace_not_included(self):
+        request = {
+            'headers': {
+                'fiware-oauth-token': 'token1',
+                'fiware-oauth-header-name': 'Authorization'
+            },
+            'workspace': None
+        }
+
+        self._test_validation_error(
+            'IdM tokens can only be inyected on Ajax requests coming from authorized widgets', request)
+
+    def test_invalid_source(self):
+        request = {
+            'headers': {
+                'fiware-oauth-token': 'token1',
+                'fiware-oauth-header-name': 'Authorization',
+                'fiware-oauth-source': 'invalid',
+            },
+            'workspace': {}
+        }
+
+        self._test_validation_error(
+            'Invalid FIWARE OAuth token source', request)
+
+    def test_missing_token(self):
+        oauth_info = MagicMock(access_token=None)
+        oauth_info.access_token_expired.return_value = True
+
+        user = MagicMock()
+        user.social_auth.get.return_value = oauth_info
+
+        request = {
+            'user': user,
+            'headers': {
+                'fiware-oauth-token': 'token1',
+                'fiware-oauth-header-name': 'Authorization'
+            },
+            'workspace': {}
+        }
+
+        self._test_validation_error(
+            'Current user has not an active FIWARE profile', request)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
